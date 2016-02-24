a5750a46fea1f059c6d0188a601b7bda18312f82
tozzifan <paolomancuso3@gmail.com>
2016-02-24 15:45:02 +0100

    "my matrix code"
    If no other git process is currently running, this probably means a
    git process crashed in this repository earlier (My pc has crashed). Make sure no other git
    process is running and remove the file manually to continue. So I try by GUI

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions allows to reduce the computational time in computing an inverse of a matrix. 
+## Generally you should use this approach whenever you have to repeat an operation a large
+## number of times (e.g. in loops).
 
-## Write a short comment describing this function
+#The first function, `makeCacheMatrix` creates a special "makeCacheMatrix", which is
+# a list of four function: set, get, setsolve and getsolve.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
+## The second function -`cacheSolve`-, which does not contain others function inside, performs 
+## a logical control to verify if the inverse of a new matrix has been already computed.
+## If the statment is true than the inverse is directly obtained from the chace, where it is 
+## stored, without computing again it.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
